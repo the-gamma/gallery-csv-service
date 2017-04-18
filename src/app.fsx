6967cb79499c88942000d18dacf9df9c01f7f5a9
Tomas Petricek <tomas@tomasp.net>
2017-04-18 16:15:40 +0100

    WiP type provider listing sources, updates

--- src/app.fsx
+++ src/app.fsx
@@ -3,148 +3,19 @@
 #r "../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 #r "../packages/Newtonsoft.Json/lib/net40/Newtonsoft.Json.dll"
 #load "../packages/FSharp.Azure.StorageTypeProvider/StorageTypeProvider.fsx"
-#load "config.fs"
-#load "pivot.fs"
+#load "config.fs" "common/serializer.fs" "pivot.fs" "storage.fs" "listing.fs" 
 #else
 module Gallery.App
 #endif
 open System
 open System.IO
-open System.Collections.Generic
-open Microsoft.WindowsAzure.Storage
 
 open Suave
 open Suave.Filters
 open Suave.Writers
 open Suave.Operators
 open FSharp.Data
-open Newtonsoft.Json
-
-
-// --------------------------------------------------------------------------------------
-// Data we store about CSV files
-// --------------------------------------------------------------------------------------
-
-type CsvFile = 
-  { id : string 
-    hidden : bool 
-    date : DateTime
-    source : string
-    description : string
-    tags : string[] 
-    passcode : string }
-  static member Create(id) = 
-    { id = id; hidden = true; date = DateTime.Today
-      source = ""; description = ""; tags = [||]; 
-      passcode = System.Guid.NewGuid().ToString("N") }
-
-// --------------------------------------------------------------------------------------
-// Saving and reading CSV files
-// --------------------------------------------------------------------------------------
-
-#if INTERACTIVE
-let createCloudBlobClient() = 
-  let account = CloudStorageAccount.Parse(Config.TheGammaSnippetsStorage)
-  account.CreateCloudBlobClient()
-#else
-let createCloudBlobClient() = 
-  let account = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("CUSTOMCONNSTR_THEGAMMASNIPS_STORAGE"))
-  account.CreateCloudBlobClient()
-#endif
-
-let serializer = JsonSerializer.Create()
-
-let toJson value = 
-  let sb = System.Text.StringBuilder()
-  use tw = new System.IO.StringWriter(sb)
-  serializer.Serialize(tw, value)
-  sb.ToString() 
-
-let fromJson<'R> str : 'R = 
-  use tr = new System.IO.StringReader(str)
-  serializer.Deserialize(tr, typeof<'R>) :?> 'R
-
-let uploadCsv count data =
-  let container = createCloudBlobClient().GetContainerReference("uploads")
-  if container.Exists() then
-    let name = sprintf "%s/file_%d.csv" (System.DateTime.Now.ToString("yyyy-MM-dd")) count
-    let blob = container.GetBlockBlobReference(name)
-    if blob.Exists() then blob.Delete() // failwithf "Blob '%s' already exists!" name
-    blob.UploadText(data, System.Text.Encoding.UTF8) 
-    CsvFile.Create(name)
-  else failwith "Container 'uploads' not found"
-
-let downloadCsv csv =
-  let container = createCloudBlobClient().GetContainerReference("uploads")
-  if container.Exists() then
-    let blob = container.GetBlockBlobReference(csv.id)
-    if not (blob.Exists()) then None
-    else Some(blob.DownloadText(System.Text.Encoding.UTF8))
-  else failwith "Container 'uploads' not found"
-
-let readMetadata () =
-  let container = createCloudBlobClient().GetContainerReference("uploads")
-  if container.Exists() then
-    let blob = container.GetBlockBlobReference("files.json")
-    if blob.Exists() then 
-      blob.DownloadText(System.Text.Encoding.UTF8) |> fromJson<CsvFile[]> 
-    else failwith "Blob 'files.json' does not exist."
-  else failwith "Container 'uploads' not found" 
-
-let writeMetadata (files:CsvFile[]) = 
-  let json = files |> toJson
-  let container = createCloudBlobClient().GetContainerReference("uploads")
-  if container.Exists() then
-    let blob = container.GetBlockBlobReference("files.json")
-    blob.UploadText(json, System.Text.Encoding.UTF8)
-  else failwith "container 'uploads' not found" 
-
-// --------------------------------------------------------------------------------------
-// Keep list of CSV files and cache recently accessed
-// --------------------------------------------------------------------------------------
-
-open Gallery.CsvService.Pivot
-
-type ParsedFile = (string * string)[] * (string * obj)[][]
-
-type Message = 
-  | UploadFile of string * AsyncReplyChannel<CsvFile>
-  | FetchFile of string * AsyncReplyChannel<option<ParsedFile>>
-
-let cache = MailboxProcessor.Start(fun inbox ->
-  let worker () = async {
-    let cache = new Dictionary<_, DateTime * _>()
-    let files = new Dictionary<_, _>()
-    for f in readMetadata () do files.Add(f.id, f)
-
-    while true do
-      let! msg = inbox.TryReceive(timeout=1000*60)
-      let remove = [ for (KeyValue(k, (t, _))) in cache do if (DateTime.Now - t).TotalMinutes > 5. then yield k ]
-      for k in remove do cache.Remove(k) |> ignore
-      match msg with
-      | None -> ()
-      | Some(UploadFile(data, repl)) ->
-          let count = files.Values |> Seq.filter (fun f -> f.date = DateTime.Today) |> Seq.length
-          let csv = uploadCsv count data
-          files.Add(csv.id, csv)
-          writeMetadata (Array.ofSeq files.Values)
-          repl.Reply(csv)
-
-      | Some(FetchFile(id, repl)) ->
-          if not (files.ContainsKey id) then repl.Reply(None) else
-          if not (cache.ContainsKey id) then
-              match downloadCsv files.[id] with
-              | Some data -> cache.Add(id, (DateTime.Now, readCsvFile data))
-              | None -> ()
-          match cache.TryGetValue id with
-          | true, (_, res) -> 
-              cache.[id] <- (DateTime.Now, res)
-              repl.Reply(Some res)
-          | _ -> repl.Reply None }
-  async { 
-    while true do
-      try return! worker ()
-      with e -> printfn "Agent failed: %A" e })
+open Gallery.CsvService
 
 // --------------------------------------------------------------------------------------
 // Server with /upload endpoint and /csv for Pivot type provider
@@ -153,31 +24,21 @@ let cache = MailboxProcessor.Start(fun inbox ->
 let app =
   choose [
     GET >=> path "/" >=> Successful.OK "Service is running..." 
-    POST >=> path "/update" >=> request (fun r ->
-        let file = fromJson<CsvFile> (System.Text.Encoding.UTF8.GetString(r.rawForm))
-        printfn "%A" file
-        Successful.OK ""
-      )
-    POST >=> path "/upload" >=> (fun ctx -> async { 
-      let data = System.Text.Encoding.UTF8.GetString(ctx.request.rawForm)
-      try 
-        ignore (readCsvFile data) 
-        let! file = cache.PostAndAsyncReply(fun ch -> UploadFile(data, ch))
-        return! Successful.OK (toJson file) ctx
-      with ParseError msg -> 
-        return! RequestErrors.BAD_REQUEST msg ctx })
-
+    POST >=> path "/update" >=> Storage.updateRecord
+    POST >=> path "/upload" >=> Storage.uploadFile
     setHeader  "Access-Control-Allow-Origin" "*"
     >=> setHeader "Access-Control-Allow-Headers" "content-type"
     >=> choose [
       OPTIONS >=> Successful.OK "CORS approved"
-      GET >=> pathScan "/csv/%s" (fun source ctx -> async {
-        let! file = cache.PostAndAsyncReply(fun ch -> FetchFile(source, ch))
+      GET >=> pathScan "/providers/listing%s" (fun _ ctx -> async {
+        let! files = Storage.getRecords ()
+        return! Listing.handleRequest files ctx })
+      GET >=> pathScan "/providers/csv/%s" (fun source ctx -> async {
+        let! file = Storage.fetchFile source
         match file with 
         | None -> return! RequestErrors.BAD_REQUEST (sprintf "File with id '%s' does not exist!" source) ctx
-        | Some (meta, data) -> return! handleRequest meta data (List.map fst ctx.request.query) ctx }) ]
+        | Some (meta, data) -> return! Pivot.handleRequest meta data (List.map fst ctx.request.query) ctx }) ]
   ]
-
 
 // When port was specified, we start the app (in Azure), 
 // otherwise we do nothing (it is hosted by 'build.fsx')
---
+++ src/listing.fs
@@ -0,0 +1,33 @@
+﻿module Gallery.CsvService.Listing
+open Gallery.CsvService
+
+open Suave
+open Suave.Filters
+open Suave.Operators
+open System
+open Gallery.CsvService.Storage
+
+let getTagId (s:string) =
+  let rec loop (sb:System.Text.StringBuilder) dash i = 
+    if i = s.Length then sb.ToString()
+    elif Char.IsLetterOrDigit s.[i] then loop (sb.Append(Char.ToLower s.[i])) false (i+1)
+    elif dash then loop sb true (i+1)
+    else loop (sb.Append '-') true (i+1)
+  loop (System.Text.StringBuilder()) true 0
+
+let handleRequest (files:CsvFile[]) = 
+  let tags = files |> Seq.collect (fun f -> f.tags) |> Seq.map (fun t -> getTagId t, t) |> dict
+  choose [
+    path "/providers/listing/" >=> request (fun r ->
+      Serializer.returnMembers [        
+        for (KeyValue(tid, t)) in tags ->
+          Member.Property(t, Result.Nested("tag/" + tid + "/"), [])
+      ])
+    pathScan "/providers/listing/tag/%s/" (fun tid ->
+      Serializer.returnMembers [
+        for file in files do
+          let hasTag = file.tags |> Seq.exists (fun t -> getTagId t = tid)
+          if hasTag then 
+            yield Member.Property(file.title, Result.Nested("/providers/listing/" + tid), [])
+      ])
+  ]
