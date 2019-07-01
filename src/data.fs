5b715326c9733aecc7c4e783808ae2836dd7e672
Tomas Petricek <tomas@tomasp.net>
2019-07-01 11:36:41 +0100

    Try to add some debugging

--- src/data.fs
+++ src/data.fs
@@ -52,19 +52,23 @@ let handleRequest root =
 
     pathScan "/providers/data/upload/%s" (fun url ctx -> async {
       use wc = new System.Net.WebClient()
-      let! file = wc.AsyncDownloadString(Uri(HttpUtility.UrlDecode(url)))
-      let! upload = Storage.Cache.uploadFile url file "uploadedCSV"
-      match upload with 
-      | Choice2Of2 msg -> return! RequestErrors.BAD_REQUEST msg ctx
-      | Choice1Of2 id ->
-          let sch = 
-            // No preview for CSV files!
-            [ ] //Schema("http://schema.org", "WebPage", ["url", JsonValue.String url ])
-              //Schema("http://schema.thegamma.net", "CompletionItem", ["hidden", JsonValue.Boolean true ]) ]
-          return! ctx |> Serializer.returnMembers [
-            Member("preview", None, Result.Nested("/null"), [], sch)
-            Member("explore", None, Result.Provider("pivot", root + "/providers/data/query/" + id), [], [])
-          ] })
+      try
+        let! file = wc.AsyncDownloadString(Uri(HttpUtility.UrlDecode(url)))
+        let! upload = Storage.Cache.uploadFile url file "uploadedCSV"
+        match upload with 
+        | Choice2Of2 msg -> return! RequestErrors.BAD_REQUEST msg ctx
+        | Choice1Of2 id ->
+            let sch = 
+              // No preview for CSV files!
+              [ ] //Schema("http://schema.org", "WebPage", ["url", JsonValue.String url ])
+                //Schema("http://schema.thegamma.net", "CompletionItem", ["hidden", JsonValue.Boolean true ]) ]
+            return! ctx |> Serializer.returnMembers [
+              Member("preview", None, Result.Nested("/null"), [], sch)
+              Member("explore", None, Result.Provider("pivot", root + "/providers/data/query/" + id), [], [])
+            ] 
+        with e -> 
+          return! ServerErrors.INTERNAL_ERROR (sprintf "FAILED Url=%s. %s" url (e.ToString())) ctx
+        })
 
     pathScan "/providers/data/query/%s" (fun id ctx -> async {
       let! file = Storage.Cache.fetchFile id
